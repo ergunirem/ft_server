78a4f109674308cc69aa34aabbf52feaaf8b5833
ergunirem <iremthedeveloper@gmail.com>
2021-02-04 11:04:35 +0300

    latest version

--- Dockerfile
+++ Dockerfile
@@ -6,42 +6,29 @@
 #    By: icikrikc <icikrikc@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2021/01/14 20:52:14 by icikrikc      #+#    #+#                  #
-#    Updated: 2021/02/01 14:53:35 by icikrikc      ########   odam.nl          #
+#    Updated: 2021/02/03 17:34:28 by icikrikc      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 FROM debian:buster
 
-# Update software packages and wget command in debian
+# Update software packages in debian
 RUN apt-get update
 RUN apt-get upgrade -y
-RUN apt-get -y install wget
 
-# Install necessary packages
+# Install necessary packages & unzip&wget command
+RUN apt-get -y install wget
+RUN apt-get -y install unzip
 RUN apt-get -y install nginx
 RUN apt-get -y install mariadb-server
-RUN apt-get -y install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip php7.3-soap php7.3-imap
+RUN apt install php-fpm php-mysql php-mbstring php-dev php-gd php-pear php-zip php-xml php-curl -y
 
-#Configure nginx
-COPY srcs/localhost /etc/nginx/sites-available/
+# Copy necessary files from your host to your image
+COPY ./srcs/init.sh ./
+COPY ./srcs/index.html ./tmp/index.html
+COPY ./srcs/nginx-conf ./tmp/nginx-conf
+COPY ./srcs/config.inc.php ./tmp/config.inc.php
+COPY ./srcs/wp-config.php ./tmp/wp-config.php
 
-#Install and cofigure phpmyadmin & wordpress
-WORKDIR /var/www/html/
-RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-english.tar.gz
-RUN tar -xf phpMyAdmin-5.0.1-english.tar.gz && rm -rf phpMyAdmin-5.0.1-english.tar.gz
-RUN mv phpMyAdmin-5.0.1-english phpmyadmin
-COPY ./srcs/config.inc.php phpmyadmin
-WORKDIR /var/www/html/
-RUN wget https://wordpress.org/latest.tar.gz
-RUN tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
-COPY ./srcs/wp-config.php /var/www/html
-
-# SSL
-RUN openssl req -x509 -nodes -days 365 -subj "/C=NL/ST=Netherlands/L=Amsterdam/O=Codam/OU=Codam/CN=icikrikc" -newkey rsa:2048 -keyout /etc/ssl/nginx-selfsigned.key -out /etc/ssl/nginx-selfsigned.crt;
-
-EXPOSE 80  443
-RUN chown -R www-data:www-data *
-RUN chmod -R 755 /var/www/*
-COPY ./srcs/init.sh .
-CMD bash init.sh
-
+EXPOSE 80 443
+ENTRYPOINT ["sh","init.sh"]
--- srcs/init.sh
+++ srcs/init.sh
@@ -1,11 +1,55 @@
-service nginx start
-service mysql start
-service php7.3-fpm start
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    init.sh                                            :+:    :+:             #
+#                                                      +:+                     #
+#    By: icikrikc <icikrikc@student.codam.nl>         +#+                      #
+#                                                    +#+                       #
+#    Created: 2021/02/03 14:03:48 by icikrikc      #+#    #+#                  #
+#    Updated: 2021/02/04 10:28:50 by icikrikc      ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
+
+# Generate website folder & create index.php & move html file
+mkdir /var/www/localhost && touch /var/www/localhost/index.php
+echo "<?php phpinfo(); ?>" >> /var/www/localhost/index.php
+mv ./tmp/index.html /var/www/localhost/index.html
+
+# Config Access
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
+
+# SSL
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=NL/ST=Netherlands/L=Amsterdam/O=Codam/OU=icikrikc/CN=localhost"
+openssl rsa -noout -text -in /etc/nginx/ssl/localhost.key
+
+# Configure nginx
+mv ./tmp/nginx-conf /etc/nginx/sites-available/localhost
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
+rm -rf /etc/nginx/sites-enabled/default
 
 # Configure mysql
+service mysql start
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+# Install & Configure phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.4/phpMyAdmin-4.9.4-all-languages.zip
+unzip phpMyAdmin-4.9.4-all-languages.zip -d /var/www/localhost/
+mv /var/www/localhost/phpMyAdmin-4.9.4-all-languages /var/www/localhost/phpmyadmin
+mv ./tmp/config.inc.php /var/www/localhost/phpmyadmin/config.inc.php
+
+# Install & Configure wordpress
+wget -c https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+mv wordpress/ /var/www/localhost
+mv ./tmp/wp-config.php /var/www/localhost/wordpress/
+
+# Boot up php & nginx
+service php7.3-fpm start
+service nginx start
 
 bash
